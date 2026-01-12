32936f651c2fea87eb0479c1cb8d8aa4bc7ad541
p4ttt0 <patricio.perezcardenal@gmail.com>
2026-01-11 23:06:13 -0300

    Feat: Audio Manager now can manage mute and unmute

--- Assets/_Project/Scripts/Audio/AudioManager.cs
+++ Assets/_Project/Scripts/Audio/AudioManager.cs
@@ -6,12 +6,20 @@ namespace FlappyBird.Audio
     {
         public static AudioManager Instance { get; private set; }
 
+        private const string MUTE_KEY = "IS_MUTED";
+
         [Header("SFX")]
         [SerializeField] private AudioClip _wing;
         [SerializeField] private AudioClip _point;
         [SerializeField] private AudioClip _hit;
         [SerializeField] private AudioClip _die;
         [SerializeField] private AudioClip _swoosh;
+
+        [Header("UI Sounds")]
+        [SerializeField] private AudioClip uiClick;
+
+        [Header("Configuration")]
+        public bool IsMuted { get; private set; }
 
         private AudioSource _audioSource;
 
@@ -26,6 +34,9 @@ namespace FlappyBird.Audio
             Instance = this;
             DontDestroyOnLoad(gameObject);
 
+            IsMuted = PlayerPrefs.GetInt(MUTE_KEY, 0) == 1;
+            ApplyVolume();
+
             _audioSource = gameObject.AddComponent<AudioSource>();
             _audioSource.playOnAwake = false;
         }
@@ -35,6 +46,7 @@ namespace FlappyBird.Audio
         public void PlayHit() => Play(_hit);
         public void PlayDie() => Play(_die);
         public void PlaySwoosh() => Play(_swoosh);
+        public void PlayUIClick() => Play(uiClick);
 
         private void Play(AudioClip clip)
         {
@@ -42,6 +54,20 @@ namespace FlappyBird.Audio
                 return;
 
             _audioSource.PlayOneShot(clip);
+        }
+
+        public void ToggleMute()
+        {
+            IsMuted = !IsMuted;
+            PlayerPrefs.SetInt(MUTE_KEY, IsMuted ? 1 : 0);
+            PlayerPrefs.Save();
+
+            ApplyVolume();
+        }
+
+        private void ApplyVolume()
+        {
+            AudioListener.volume = IsMuted ? 0f : 1f;
         }
     }
 }
